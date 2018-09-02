2387ac25dd10bd6fd828e1fedea06b209a0f59ad
Lumart Chryssomallis <lumartch@gmail.com>
2018-09-02 13:16:52 -0500

    Implementación de Ethernet - Actualizar a Ipv4

---
+++ Ethernet.h
@@ -0,0 +1,27 @@
+#ifndef ETHERNET_H
+#define ETHERNET_H
+
+#define DIRECCION 6
+#define PROTOCOLO 2
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+
+class Ethernet {
+public:
+    Ethernet();
+    std::string getDireccionDestino();
+    std::string getDireccionOrigen();
+    std::string getTipoProtocolo();
+    bool abrirArchivo(const std::string&archivo);
+
+private:
+    std::string direccionDestino;
+    std::string direccionOrigen;
+    std::string tipoProtocolo;
+    void setDireccion(std::fstream&archivo, std::string&dir);
+    void setProtocolo(std::fstream&archivo);
+};
+
+#endif // ETHERNET_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include "Ethernet.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    string archivo;
+    Ethernet eth;
+    cout << "---------Sniffer---------" << endl;
+    cout << "Ingrese el nombre del archivo: ";
+    getline(cin, archivo);
+    if (!eth.abrirArchivo(archivo)){
+        cout << "No fue posible abrir el archivo deseado." << endl;
+        return 0;
+    }
+    cout << "¡Archivo abierto correctamente!" << endl;
+    cout << "Dirección destino: " << eth.getDireccionDestino() << endl;
+    cout << "Dirección origen: " << eth.getDireccionOrigen() << endl;
+    cout << "Tipo de protocolo: " << eth.getTipoProtocolo() << endl;
+    if(eth.getTipoProtocolo() == "0800"){
+
+    }
+    else if(eth.getTipoProtocolo() == "0806"){
+
+    }
+    else if(eth.getTipoProtocolo() == "8035"){
+
+    }
+    else{
+
+    }
     return 0;
 }
