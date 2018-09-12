46a36b858ce9e94e3e94ae47c15e2f516422644b
Lumart Chryssomallis <lumartch@gmail.com>
2018-09-11 21:03:43 -0500

    Implementación ARP- RARP

---
+++ arpRarp.h
@@ -0,0 +1,56 @@
+#ifndef ARPRARP_H_INCLUDED
+#define ARPRARP_H_INCLUDED
+
+#define INICIOARPRARP 14
+#define BYTE   1
+#define BITS0  0
+#define BITS4  4
+#define BITS3  3
+#define BITS6  6
+#define BITS8  8
+#define DIRECCION 6
+
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <bitset>
+#include <cmath>
+
+class ArpRarp {
+public:
+	ArpRarp();
+	ArpRarp(const std::string&archivo);
+	void abrirArchivo(const std::string&archivo);
+	std::string getTipoHardware();
+	std::string getTipoProtocolo();
+	std::string getLongitudMac();
+	std::string getLongitudIp();
+	std::string getCodigoOperacion();
+	std::string getMacEmisor();
+	std::string getIpEmisor();
+	std::string getMacReceptor();
+	std::string getIpReceptor();
+
+private:
+	std::string tipoHardware;
+	std::string tipoProtocolo;
+	std::string longitudMac;
+	std::string longitudIp;
+	std::string codigoOperacion;
+	std::string macEmisor;
+	std::string ipEmisor;
+	std::string macReceptor;
+	std::string ipReceptor;
+	int binarioToDecimal(const std::string&bin);
+	std::string hexadecimalToBinario(std::fstream&archivo);
+	std::string separarBinario(const std::string&bin, const int &limInf, const int &limSup);
+	std::string getTipoHardwareCadena(const std::string& codigo);
+	void setProtocolo(std::fstream&archivo);
+	std::string getProtocoloCadena();
+	std::string getCodigoOperacionCadena(const std::string& codigo);
+	void setDireccion(std::fstream&archivo, std::string&dir);
+	void formatoIp(std::string&ip, std::fstream& archivo);
+};
+
+#endif // ARPRARP_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Ethernet.h"
 #include "ipv4.h"
 #include "ICMPv4.h"
+#include "arpRarp.h"
 
 #if _WIN32
 #define CLEAR "cls"
@@ -12,62 +13,78 @@
 using namespace std;
 
 int main() {
-    string archivo, opc;
-    do{
-        system(CLEAR);
-        cout << "---------Sniffer---------";
-        cout << endl << "Ingrese el nombre del archivo: ";
-        getline(cin, archivo);
-        Ethernet eth;
-        if (!eth.abrirArchivo(archivo)){
-            cout << "No fue posible abrir el archivo deseado. ¿Desea intentarlo de nuevo? (S/N): " << endl;
-        }
-        else{
-            cout << endl;
-            cout << "¡Archivo abierto correctamente!" << endl << endl;
-            cout << "Dirección destino: " << eth.getDireccionDestino() << endl;
-            cout << "Dirección origen: " << eth.getDireccionOrigen() << endl;
-            cout << "Tipo de protocolo: " << eth.getTipoProtocolo();
-            if(eth.getTipoProtocolo() == "08:00"){
-                cout << " - Ipv4" << endl << endl;
-                Ipv4 ipv4(archivo);
-                cout << "Version: " << ipv4.getVersion() << endl;
-                cout << "Longitud de cabecera: " << ipv4.getLongitudCabecera() << endl;
-                cout << "Servicios: " << ipv4.getServicio() << endl;
-                cout << "Desglose de bits: " << ipv4.getDesgloseDeBits() << endl;
-                cout << "Longitud total: " << ipv4.getLongitudTotal() << endl;
-                cout << "Identificador: " << ipv4.getIdentificador() << endl;
-                cout << "Flags: " << ipv4.getFlags() << endl;
-                cout << "Posición del fragmento: " << ipv4.getPosicionFragmento() << endl;
-                cout << "Tiempo de vida: " << ipv4.getTiempoDeVida() << endl;
-                cout << "Protocolo: " << ipv4.getTipoProtocolo() << endl;
-                cout << "Suma de cabecera: " << ipv4.getSumaCabecera() << endl;
-                cout << "Ip Origen: " << ipv4.getIpOrigen() << endl;
-                cout << "Ip Destino: " << ipv4.getIpDestino() << endl;
-                if(ipv4.getTipoProtocolo() == "ICMP"){
-                    ICMPv4 icmp(archivo);
-                    cout << endl << endl << "-----ICMP-----" << endl;
-                    cout << "Mensaje informativo: " << icmp.getMensajeInformativo() << endl;
-                    cout << "Mensaje mensaje de error: " << icmp.getMensajeError() << endl;
-                    cout << "Checksum: " << icmp.getChecksum() << endl;
-                }
-            }
-            else if(eth.getTipoProtocolo() == "08:06"){
-                cout << " - ARP" << endl << endl;
-            }
-            else if(eth.getTipoProtocolo() == "80:35"){
-                cout << " - RARP" << endl << endl;
-            }
-            else{
-                cout << " - Ipv6" << endl << endl;
-            }
-            cout << endl << endl << "¿Desea abrir un nuevo archivo?(S/N): ";
-        }
-        do{
-            getline(cin, opc);
-        }while(opc != "N" and opc != "n" and
-               opc != "S" and opc != "s");
-    }while(opc == "S" or opc == "s");
-    cout << endl << "Gracias por usar el analizador de paquetes 'Sniffer'.";
-    return 0;
+	string archivo, opc;
+	do {
+		system(CLEAR);
+		cout << "---------Sniffer---------";
+		cout << endl << "Ingrese el nombre del archivo: ";
+		getline(cin, archivo);
+		Ethernet eth;
+		if (!eth.abrirArchivo(archivo)) {
+			cout << "No fue posible abrir el archivo deseado. ¿Desea intentarlo de nuevo? (S/N): " << endl;
+		} else {
+			cout << endl;
+			cout << "¡Archivo abierto correctamente!" << endl << endl;
+			cout << "Dirección destino: " << eth.getDireccionDestino() << endl;
+			cout << "Dirección origen: " << eth.getDireccionOrigen() << endl;
+			cout << "Tipo de protocolo: " << eth.getTipoProtocolo();
+			if(eth.getTipoProtocolo() == "08:00") {
+				cout << " - Ipv4" << endl << endl;
+				Ipv4 ipv4(archivo);
+				cout << "Version: " << ipv4.getVersion() << endl;
+				cout << "Longitud de cabecera: " << ipv4.getLongitudCabecera() << endl;
+				cout << "Servicios: " << ipv4.getServicio() << endl;
+				cout << "Desglose de bits: " << ipv4.getDesgloseDeBits() << endl;
+				cout << "Longitud total: " << ipv4.getLongitudTotal() << endl;
+				cout << "Identificador: " << ipv4.getIdentificador() << endl;
+				cout << "Flags: " << ipv4.getFlags() << endl;
+				cout << "Posición del fragmento: " << ipv4.getPosicionFragmento() << endl;
+				cout << "Tiempo de vida: " << ipv4.getTiempoDeVida() << endl;
+				cout << "Protocolo: " << ipv4.getTipoProtocolo() << endl;
+				cout << "Suma de cabecera: " << ipv4.getSumaCabecera() << endl;
+				cout << "Ip Origen: " << ipv4.getIpOrigen() << endl;
+				cout << "Ip Destino: " << ipv4.getIpDestino() << endl;
+				if(ipv4.getTipoProtocolo() == "ICMP") {
+					ICMPv4 icmp(archivo);
+					cout << endl << endl << "-----ICMP-----" << endl;
+					cout << "Mensaje informativo: " << icmp.getMensajeInformativo() << endl;
+					cout << "Mensaje mensaje de error: " << icmp.getMensajeError() << endl;
+					cout << "Checksum: " << icmp.getChecksum() << endl;
+				}
+			} else if(eth.getTipoProtocolo() == "08:06") {
+				cout << " - ARP" << endl << endl;
+				ArpRarp arpRarp(archivo);
+				cout << "Tipo Hardware: "  << arpRarp.getTipoHardware()    << endl;
+				cout << "Tipo Protocolo: " << arpRarp.getTipoProtocolo()   << endl;
+				cout << "Longitud Mac: "   << arpRarp.getLongitudMac()     << endl;
+				cout << "Longitud Ip: "    << arpRarp.getLongitudIp()      << endl;
+				cout << "Codigo Operacion: " << arpRarp.getCodigoOperacion() << endl;
+				cout << "Mac Emisor: "     << arpRarp.getMacEmisor()       << endl;
+				cout << "Ip Emisor: "      << arpRarp.getIpEmisor()        << endl;
+				cout << "Mac Receptor: "   << arpRarp.getMacReceptor()     << endl;
+				cout << "Ip Receptor: "    << arpRarp.getIpReceptor()      << endl;
+			} else if(eth.getTipoProtocolo() == "80:35") {
+				cout << " - RARP" << endl << endl;
+				ArpRarp arpRarp(archivo);
+				cout << "Tipo Hardware: "  << arpRarp.getTipoHardware()    << endl;
+				cout << "Tipo Protocolo: " << arpRarp.getTipoProtocolo()   << endl;
+				cout << "Longitud Mac: "   << arpRarp.getLongitudMac()     << endl;
+				cout << "Longitud Ip: "    << arpRarp.getLongitudIp()      << endl;
+				cout << "Codigo Operacion: " << arpRarp.getCodigoOperacion() << endl;
+				cout << "Mac Emisor: "     << arpRarp.getMacEmisor()       << endl;
+				cout << "Ip Emisor: "      << arpRarp.getIpEmisor()        << endl;
+				cout << "Mac Receptor: "   << arpRarp.getMacReceptor()     << endl;
+				cout << "Ip Receptor: "    << arpRarp.getIpReceptor()      << endl;
+			} else {
+				cout << " - Ipv6" << endl << endl;
+			}
+			cout << endl << endl << "¿Desea abrir un nuevo archivo?(S/N): ";
+		}
+		do {
+			getline(cin, opc);
+		} while(opc != "N" and opc != "n" and
+		        opc != "S" and opc != "s");
+	} while(opc == "S" or opc == "s");
+	cout << endl << "Gracias por usar el analizador de paquetes 'Sniffer'.";
+	return 0;
 }
